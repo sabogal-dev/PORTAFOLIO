7ff6e2f77ea257806c2d8519c5aa3b8c3c28ead1
emilio <sabogaldavid26@gmail.com>
2025-06-08 18:19:58 -0500

    links acualizados

--- src/components/proyectos.jsx
+++ src/components/proyectos.jsx
@@ -5,20 +5,27 @@ import "../styles/proyectos.css"
 const personas = [
     {
         id: 1,
-        titulo: "FastPay",
-        descripcion: "Aplicacion fullstack conectada a la Api de Odoo de la empresa, consulta facturas para que asesores comerciales calculen descuentos para sus clientes y muestra un historial de pagos de cada factura, agilizando un 30% los procesos",
-        imagen: "https://sabogal.tech/CDNportafolio/aprovehaDescuento.png",
-        url: "https://sabogal.top"
-    },
-    {
-        id: 2,
-        titulo: "Dashboard CODE",
-        descripcion: "Visualiza estadísticas, productos y objetivos con React, Tailwind y Tremor Charts. Herramienta clave para el seguimiento empresarial y la toma de decisiones.",
-        imagen: "https://themes.getbootstrap.com/wp-content/uploads/2020/09/front-dashboard-v2-cover.jpeg",
+        titulo: "Dashboard interactivo CODE",
+        descripcion: "Visualiza métricas clave como ventas, desempeño por vendedor y productos por categoría. Construido con React, Chakra UI y Express para ofrecer datos en tiempo real y facilitar decisiones estratégicas.",
+        imagen: "https://sabogal.tech/CDNportafolio/dashboard.png",
         url: "https://sabogal.top/dashboard/"
     },
     {
-        id: 3,
+        id: 2,
+        titulo: "FastPay",
+        descripcion: "Aplicacion fullstack conectada a la Api de Odoo de la empresa, consulta facturas para que asesores comerciales calculen descuentos para sus clientes y muestra un historial de pagos de cada factura, agilizando un 30% los procesos",
+        imagen: "https://sabogal.tech/CDNportafolio/aprovehaDescuento.png",
+        url: "https://sabogal.top/calculadoraCode/"
+    },
+    // {
+    //     id: 3,
+    //     titulo: 'inventario',
+    //     descripcion: 'Sistema de inventario, Crud en React conectado a supabase',
+    //     imagen: '',
+    //     url: '',
+    // },
+    {
+        id: 4,
         titulo: "WorkoutApp",
         descripcion: "Mejora tus entrenamietos con una agenda dedicada al Gym, multiples estadisticas, conectada a Supabase para BD y autenticacion de usuarios",
         imagen: "https://sabogal.tech/CDNportafolio/workoutapp.png",
